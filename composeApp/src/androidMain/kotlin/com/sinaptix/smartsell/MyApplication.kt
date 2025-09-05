4d7dc6c3f2c88fe120bc0717244dccb649703e0a
FrancoMerCas <franco.mercado.c@gmail.com>
2025-09-04 22:05:24 -0600

    add DI with koin
    add DI with koin

--- composeApp/src/androidMain/kotlin/com/sinaptix/smartsell/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/com/sinaptix/smartsell/MyApplication.kt
@@ -1,12 +1,19 @@
 package com.sinaptix.smartsell
 
 import android.app.Application
+import com.sinaptix.smartsell.di.initializeKoin
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.initialize
+import org.koin.android.ext.koin.androidContext
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initializeKoin(
+            config = {
+                androidContext(this@MyApplication)
+            }
+        )
         Firebase.initialize(context = this)
     }
 }
